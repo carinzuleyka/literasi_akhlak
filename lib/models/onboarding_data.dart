61dc1c87ade754a1764e67d2641b4a48c06a8368
Carin Zuleyka <czuleyka76@gmail.com>
2025-08-05 14:24:36 +0700

    foto

--- lib/models/onboarding_data.dart
+++ lib/models/onboarding_data.dart
@@ -12,23 +12,18 @@ class OnboardingData {
 
 List<OnboardingData> onboardingPages = [
   OnboardingData(
-    title: "",
-    description: "",
-    imagePath: "assets/images/onboarding1.png",
-  ),
-  OnboardingData(
     title: "Temukan Dunia Membaca & Inspirasi Baru",
     description: "Jelajahi berbagai artikel, resensi buku, dan konten yang memberkaya wawasanmu setiap hari.",
-    imagePath: "assets/images/onboarding2.png",
+    imagePath: "assets/images/asset1.png",
   ),
   OnboardingData(
     title: "Belajar Bersama & Tingkatkan Minat Baca",
     description: "Gabung dengan komunitas literasi, diskusikan ide, dan temukan rekomendasi bacaan menarik.",
-    imagePath: "assets/images/onboarding3.png",
+    imagePath: "assets/images/asset2.png",
   ),
   OnboardingData(
     title: "Nikmati Literasi Secara Interaktif",
     description: "Dapatkan pengalaman membaca yang menyenangkan dengan artikel pilihan, fitur resensi, dan video edukasi.",
-    imagePath: "assets/images/onboarding4.png",
+    imagePath: "assets/images/asset3.png",
   ),
 ];
